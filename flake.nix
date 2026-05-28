5f69d5f03f85d3b62a2f9e3324be5de2e56bc52f
gokhanpicgeta <vishnuprasad97@hotmail.com>
2026-05-28 10:22:32 +1000

    Added flake nix package

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "Silverpond Factory CLI";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        python = pkgs.python311;
+
+        silverpond-factory = python.pkgs.buildPythonPackage {
+          pname = "silverpond-factory";
+          version = "0.1.0";
+          src = ./.;
+          pyproject = true;
+
+          build-system = with python.pkgs; [ setuptools ];
+
+          dependencies = with python.pkgs; [
+            typer
+            rich
+            pydantic
+            pyyaml
+            questionary
+            slack-sdk
+          ];
+        };
+      in {
+        packages.default = silverpond-factory;
+
+        apps.default = {
+          type = "app";
+          program = "${silverpond-factory}/bin/factory";
+        };
+
+        devShells.default = pkgs.mkShell {
+          packages = [
+            (python.withPackages (ps: with ps; [
+              typer rich pydantic pyyaml questionary slack-sdk
+            ]))
+          ];
+        };
+      }
+    );
+}
